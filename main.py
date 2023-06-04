7619be113a3966fb0f68e7e96b931b22ba1b1c18
Kostianytn Gurianov <kguryanov@gmail.com>
2023-06-04 09:45:06 +0300

    fixing some pylint errors

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fizzbuzz import fizzbuzz
 
 if __name__ == '__main__':
-    print(list(fizzbuzz(1, 50)))
-
-    # for idx, value in fizzbuzz(1, 50):
-    #     print(f"{idx} : {value}")
+    for idx, value in fizzbuzz(50):
+        print(f"{idx} : {value}")
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -2,18 +2,23 @@ import pytest as pytest
 
 from fizzbuzz import DEFAULT_FIZBUZZ, get_fizzbuzz, fizzbuzz
 
-EXPECTED_10 = (10, list(enumerate([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz"],
+EXPECTED_10 = (10, list(enumerate([1, 2, "Fizz", 4, "Buzz", "Fizz",
+                                   7, 8, "Fizz", "Buzz"],
                                   start=1)))
-EXPECTED_15 = (15, list(enumerate([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8,
-                                   "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"],
+EXPECTED_15 = (15, list(enumerate([1, 2, "Fizz", 4, "Buzz",
+                                   "Fizz", 7, 8,
+                                   "Fizz", "Buzz", 11,
+                                   "Fizz", 13, 14, "FizzBuzz"],
                                   start=1)))
 
-EXPECTED_50 = (50, list(enumerate([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz",
-                                   "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16, 17,
-                                   "Fizz", 19, "Buzz", "Fizz", 22, 23, "Fizz", "Buzz",
-                                   26, "Fizz", 28, 29, "FizzBuzz", 31, 32, "Fizz", 34, "Buzz",
-                                   "Fizz", 37, 38, "Fizz", "Buzz", 41, "Fizz", 43, 44,
-                                   "FizzBuzz", 46, 47, "Fizz", 49, "Buzz"], start=1)))
+EXPECTED_50 = (50, list(enumerate([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8,
+                                   "Fizz","Buzz", 11, "Fizz", 13, 14,
+                                   "FizzBuzz", 16, 17, "Fizz", 19, "Buzz",
+                                   "Fizz", 22, 23, "Fizz", "Buzz", 26, "Fizz",
+                                   28, 29, "FizzBuzz", 31, 32, "Fizz", 34,
+                                   "Buzz", "Fizz", 37, 38, "Fizz", "Buzz",
+                                   41, "Fizz", 43, 44,"FizzBuzz", 46, 47,
+                                   "Fizz", 49, "Buzz"], start=1)))
 
 EXPECTED_START_1_1 = (1, 1, [(1, 1)])
 EXPECTED_START_1_10 = (1, 10,
